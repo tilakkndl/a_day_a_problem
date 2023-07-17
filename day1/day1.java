006cb620cb7930e05f3c04212da4ae7a0b52db7d
Tilak Kandel <tilakkndl2002@gmail.com>
2023-07-17 22:34:44 +0545

    First day simple sum problem

---
+++ day1/day1.java
@@ -0,0 +1,11 @@
+package day1;
+
+class day1 {
+
+    static int sum(int num1, int num2) {
+        return num1+num2;
+    }
+    public static void main (String [] args){
+        System.out.println(sum(2, 3));
+    }
+};
